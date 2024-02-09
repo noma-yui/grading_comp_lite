ac65e64b78531a68ad51a119da004327adc95876
Yui Noma <yuhiir@gmail.com>
2024-02-09 15:22:17 +0900

    add listfiles function

---
+++ util/misc.py
@@ -0,0 +1,27 @@
+import glob
+import os
+from pathlib import Path
+
+def listfiles(rootdir, ext=None):
+    """List all files in some directory.
+
+    Example:
+        filelist = listfiles("./", ".py")
+    Args:
+        rootdir (str | pathlib.Path): directory name 
+        ext (str, optional): file extention which you want to list.
+            Defaults to None.
+
+    Returns:
+        list(pathlib.Path): list of files
+    """
+    adir = Path(rootdir).absolute()
+    
+    if ext is None:
+        filelist = list(adir.glob("**/*"))
+    else:
+        filelist = list(adir.glob("**/*{}".format(ext)))
+
+    return list(filter(lambda p: p.is_file(), filelist))
+
+
